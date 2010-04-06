77a5e3abda2f0aeee7c5f2b2ad8ff22dbdfe19dc
thiagotn <thiagotn@e3d7a436-962f-8198-dcdd-8637d543aa78>
2010-04-06 22:25:51 +0000

    Primeiro Commit.

---
+++ agenda/src/br/com/agenda/jdbc/dao/ContatoDAO.java
@@ -0,0 +1,135 @@
+package br.com.agenda.jdbc.dao;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import br.com.agenda.jdbc.FabricaDeConexao;
+import br.com.agenda.modelo.Contato;
+
+public class ContatoDAO {
+
+	private Connection connection;
+	
+	public ContatoDAO()  {
+		try {
+			this.connection = FabricaDeConexao.getConexaoMySQL();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	public void adiciona(Contato contato) {
+		String sql = "insert into contatos (nome, email, endereco, dataNascimento) values (?,?,?,?)";
+		
+		try {
+			// prepared statment para inserção
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			
+			// seta os valores
+			stmt.setString(1, contato.getNome());
+			stmt.setString(2, contato.getEmail());
+			stmt.setString(3, contato.getEndereco());
+			stmt.setDate(4, new Date( contato.getDataNascimento().getTimeInMillis() ));
+			
+			// executa
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			throw new RuntimeException();
+		}
+	}
+	
+	public List<Contato> getLista(){
+
+		try {
+			List<Contato> contatos = new ArrayList<Contato>();
+			PreparedStatement stmt = this.connection.prepareStatement("select * from contatos");
+			ResultSet rs = stmt.executeQuery();
+			
+			while(rs.next()) {
+				// Criando o contato
+				Contato contato = new Contato();
+				contato.setId(rs.getInt("id"));
+				contato.setNome(rs.getString("nome"));
+				contato.setEmail(rs.getString("email"));
+				contato.setEndereco(rs.getString("endereco"));
+				
+				Calendar  data = Calendar.getInstance();
+				data.setTime(rs.getDate("dataNascimento"));
+				contato.setDataNascimento(data); 
+				
+				// Adicionando o objeto a Lista
+				contatos.add(contato);
+			}
+			rs.close();
+			stmt.close();
+			return contatos;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void altera(Contato contato) {
+		String sql = "update contatos set nome=?, email=?, endereco=?, dataNascimento=? where id=?";
+		
+		try {
+			 PreparedStatement stmt = connection.prepareStatement(sql);
+			 stmt.setString(1, contato.getNome());
+			 stmt.setString(2, contato.getEmail());
+			 stmt.setString(3, contato.getEndereco());
+			 stmt.setDate(4, new Date(contato.getDataNascimento().getTimeInMillis()));
+			 stmt.setLong(5, contato.getId());
+			 stmt.execute();
+			 stmt.close();
+		} catch (SQLException e) {
+		 throw new RuntimeException(e);
+		}
+	}
+	
+	public void remove(Contato contato) {
+		try {
+			PreparedStatement stmt = connection.prepareStatement("delete from contatos where id=?");
+			stmt.setLong(1, contato.getId());
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public Contato pesquisar(int id) {
+		try {
+			PreparedStatement stmt = this.connection.prepareStatement("select * from contatos where id=?");
+			stmt.setInt(1, id);
+			ResultSet rs = stmt.executeQuery();
+			
+			if(rs.next()) {
+				// Criando o contato
+				Contato contato = new Contato();
+				contato.setId(rs.getInt("id"));
+				contato.setNome(rs.getString("nome"));
+				contato.setEmail(rs.getString("email"));
+				contato.setEndereco(rs.getString("endereco"));
+				
+				Calendar  data = Calendar.getInstance();
+				data.setTime(rs.getDate("dataNascimento"));
+				contato.setDataNascimento(data); 
+					
+				rs.close();
+				stmt.close();
+				return contato;
+			}
+			return null;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
---
+++ agenda/src/br/com/agenda/servlet/AdicionaContatoServlet.java
@@ -0,0 +1,56 @@
+package br.com.agenda.servlet;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.agenda.jdbc.dao.ContatoDAO;
+import br.com.agenda.modelo.Contato;
+
+@SuppressWarnings("serial")
+public class AdicionaContatoServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+			
+		String nome = request.getParameter("nome");
+		String email = request.getParameter("email");
+		String endereco =request.getParameter("endereco");
+		
+		String dataEmTexto = request.getParameter("dataNascimento");
+		Calendar dataNascimento = null;
+		
+		// fazendo a conversão da data
+		try {
+			Date date = new SimpleDateFormat("dd/MM/yyyy").parse(dataEmTexto);
+			dataNascimento = Calendar.getInstance();
+			dataNascimento.setTime(date);
+		} catch (ParseException e) {
+			System.out.println("Erro ao converter a data");
+			return; // retorna para a execução do método
+		}
+		
+		// Instancia Contato
+		Contato contato = new Contato();
+		contato.setNome(nome);
+		contato.setEmail(email);
+		contato.setEndereco(endereco);
+		contato.setDataNascimento(dataNascimento);
+		
+		// Instancia ContatoDAO
+		ContatoDAO dao = new ContatoDAO();
+		dao.adiciona(contato);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/lista-contato.view");
+		rd.forward(request, response);
+	}
+}
