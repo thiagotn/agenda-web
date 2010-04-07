b1586617728791f0ce4212cb3a90e3156a29c37a
thiagotn <thiagotn@e3d7a436-962f-8198-dcdd-8637d543aa78>
2010-04-07 02:04:24 +0000

    Implementado filtro para forçar autenticação de usuários.

---
+++ trunk/agenda/src/br/com/agenda/servlet/FormContatoServlet.java
@@ -0,0 +1,20 @@
+package br.com.agenda.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class FormContatoServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/contato/form.jsp");
+		rd.forward(request, response);
+	}
+}
---
+++ trunk/agenda/src/br/com/agenda/web/filtro/Autorizador.java
@@ -0,0 +1,53 @@
+package br.com.agenda.web.filtro;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import br.com.agenda.modelo.Usuario;
+
+public class Autorizador implements Filter {
+
+	private String urlRequisitada;
+	
+	@Override
+	public void destroy() {
+		System.out.println("Finalizando Filtro Autorizador");
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		
+		HttpSession session = httpRequest.getSession();
+		
+		urlRequisitada = httpRequest.getRequestURI();
+		
+		if((!urlRequisitada.endsWith(".view")) || (urlRequisitada.equals("/agenda/autenticacao.view"))){
+			chain.doFilter(request, response);
+		} else {
+			Usuario usuario = (Usuario) session.getAttribute("usuarioWeb");
+			if(usuario != null) {
+				chain.doFilter(request, response);
+			} else {
+				httpResponse.sendRedirect("/agenda/login.jsp");
+			}	
+		}
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		System.out.println("Iniciando Filtro Autorizador");
+	}
+}
