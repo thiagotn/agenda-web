c8213e2ff5b4c79158715b85f4ee9ccf052c49f1
thiagotn <thiagotn@e3d7a436-962f-8198-dcdd-8637d543aa78>
2010-04-09 00:02:40 +0000

    Classes necessários para enviar email.

---
+++ trunk/agenda/src/br/com/agenda/mail/Mensagem.java
@@ -0,0 +1,32 @@
+package br.com.agenda.mail;
+
+public class Mensagem {
+	
+	private String para;
+	private String assunto;
+	private String mensagem;
+
+	public String getPara() {
+		return para;
+	}
+
+	public void setPara(String para) {
+		this.para = para;
+	}
+
+	public String getAssunto() {
+		return assunto;
+	}
+
+	public void setAssunto(String assunto) {
+		this.assunto = assunto;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+}
